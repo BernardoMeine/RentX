27cd91aa9aabd701a44f5592818e3666f3fa8fde
Bernardo Meine <bemeine15@hotmail.com>
2022-12-22 18:59:05 -0300

    refactoring

--- src/modules/cars/useCases/profileUserUseCase/ProfileUserController.ts
+++ src/modules/cars/useCases/profileUserUseCase/ProfileUserController.ts
@@ -1,8 +1,14 @@
 import { Request, Response } from "express"
+import { container } from "tsyringe";
+import { ProfileUserUseCase } from "./ProfileUserUseCase";
 
 
 class ProfileUserController {
-  async handle(req: Request, res: Response): Promise<Response>
+  async handle(req: Request, res: Response): Promise<Response> {
+    const profileUserUseCase = container.resolve(ProfileUserUseCase)
+
+    return
+  }
 }
 
 export { ProfileUserController }
--- src/modules/cars/useCases/profileUserUseCase/ProfileUserUseCase.ts
+++ src/modules/cars/useCases/profileUserUseCase/ProfileUserUseCase.ts
@@ -1,8 +1,17 @@
+import { User } from "@modules/accounts/infra/typeorm/entities/User";
+import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
+import { inject, injectable } from "tsyringe";
 
-
-
+@injectable()
 class ProfileUserUseCase {
+  constructor(
+    @inject("UserRepository")
+    private usersRepository: IUsersRepository
+  ) { }
 
+  async execute(id: string): Promise<User> {
+    const userId = await this.usersRepository.findById(id);
+  }
 }
 
 export { ProfileUserUseCase } 
