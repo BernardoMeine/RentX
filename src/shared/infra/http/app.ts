c3b6b9e25fef454e265b07078afa41900471ee8c
Bernardo Meine <bemeine15@hotmail.com>
2023-01-10 17:50:52 -0300

    adding cors

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -2,12 +2,13 @@ import 'reflect-metadata';
 import "dotenv/config";
 import express from 'express';
 import { Request, Response, NextFunction } from 'express';
-import 'express-async-errors'
-import swaggerUi from 'swagger-ui-express'
+import 'express-async-errors';
+import swaggerUi from 'swagger-ui-express';
+import cors from 'cors';
 
-import "@shared/container"
+import "@shared/container";
 import { AppError } from '@shared/errors/AppErrors';
-import createConnection from "@shared/infra/typeorm"
+import createConnection from "@shared/infra/typeorm";
 
 import swaggerFile from '../../../swagger.json';
 import { router } from './routes';
@@ -24,6 +25,7 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
 app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 
+app.use(cors());
 app.use(router);
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
